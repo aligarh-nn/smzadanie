99db1baaf32f9f9f7aea13192a7ad145bdca1eff
aligarh-nn <s.zaytsev.nn@gmail.com>
2025-07-23 14:50:28 +0300

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,1 +1,10 @@
+# config.py
+
+BOT_TOKEN = "8049424222:AAFhKM19Q9eCxDd9hq2LPBxKAAOA1QnNvC4"
+
+# ID Google App Script вебхука или публичного Web App Яндекс Таблицы
+YANDEX_SCRIPT_URL = "https://disk.yandex.ru/edit/d/qZ6wgkMUzF8tTqTRUh0hHCPegnqahzm72s0qoIz-cKg6NEZsVmNkdTd0QQ?source=docs&sk=y7e8b4fe3fd70bfa7e5c555d12361f888"
+
+# Пример:
+# YANDEX_SCRIPT_URL = "https://script.googleusercontent.com/macros/s/AKfycbw1234567890abcdef/exec"
 
